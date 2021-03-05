60d64f78a19fe6d17ab73877a975e706b91ea73f
Arriv9l <imaginistx12@gmail.com>
2021-03-05 15:42:12 +0800

    :construction_worker: Update CI

--- part1/1.2/src/index.js
+++ part1/1.2/src/index.js
@@ -28,7 +28,7 @@ const Total = (props) => (
 )
 
 const App = () => {
-  const course = 'Half Stack application development'
+  const course = 'Half Stack application development🆕'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
