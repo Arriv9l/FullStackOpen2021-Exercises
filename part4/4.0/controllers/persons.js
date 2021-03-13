4840baf5f6dda70d042c8d3abfa8d76055dfc1fa
Arriv9l <imaginistx12@gmail.com>
2021-03-13 13:50:49 +0800

    :new: Complete Exercises 4.1~4.2

---
+++ part4/4.0/controllers/persons.js
@@ -0,0 +1,64 @@
+const personsRouter = require('express').Router()
+const Person = require('../models/person')
+
+personsRouter.get('/', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+})
+
+personsRouter.get('/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+personsRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+personsRouter.delete('/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+personsRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  // 添加了可选的代码 { new: true } 参数，这将导致使用新修改的文档而不是原始文档调用事件处理程序
+  // 在更新操作中，mongoose 验证默认是关闭的，需要加上 { runValidators: true }
+  // 根据 https://liuzhenglai.com/post/5dbd385f8dea5b6b578765d9
+  // 由于修改了 _id 为 id，所以不能正确识别，需要加上 { context: 'query' }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = personsRouter
